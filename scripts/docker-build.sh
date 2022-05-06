5b714b22d1a7ec81a6f12d2cdd2426e24246d88e
Aleksandr Soloshenko <capcom2me@gmail.com>
2022-05-06 23:21:57 +0700

    Fixed: git tag extraction

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TAG=$(git tag)
+TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
 if [ -z "$TAG" ]; then
     exit 0
 fi
